54ebc5c58474e691181d64f7e33b87854058b728
Roman <romansp007@mail.ru>
2023-10-19 23:23:14 +0300

    Сохраняю

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 10.Напишите программу, которая принимает на вход трёхзначное 
+//число и на выходе показывает вторую цифру этого числа
+Console.Write("Ведите трехзначное число ");
+string firstNamber = Console.ReadLine();
+int number;
+bool isNumber = int.TryParse(firstNamber, out number);
+int result = (number % 100) / 10;
+   
+Console.Write(result);
